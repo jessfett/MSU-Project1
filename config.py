637ec303a856809965864e26441f7f4c7d61e12d
jessfett <69079780+jessfett@users.noreply.github.com>
2020-10-10 10:37:03 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,45 +1,4 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [],
-   "source": [
+api_key="AIzaSyDoDuZ9Shbs_WPbVnmTtuAy3Wwb_dvkfMU"
 
-    "# api_key\n",
-    "api_key=\"e9f34fe30c3508b1804ca865ed96b3b4\"\n",
-    "\n"
 
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+    
